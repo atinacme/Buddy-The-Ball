81352a81c15dcdbd1a38881ebda6ca02cc8d5ca1
atinacme <atingupta@acmeintech.in>
2022-11-17 16:49:53 +0530

    frontend and backend directories added 17/11/2022

--- BuddyTheBall-FrontEnd/src/auth/Login.js
+++ BuddyTheBall-FrontEnd/src/auth/Login.js
@@ -5,7 +5,7 @@ import buddy from '../assets/buddy.png';
 export default function Login({ navigation }) {
     return (
         <SafeAreaView style={styles.wrapper}>
-            <Image source={buddy} style={{ width: 200, height: 100, marginLeft: 'auto', marginRight: 'auto' }}  />
+            <Image source={buddy} style={{ width: 200, height: 100, marginLeft: 'auto', marginRight: 'auto' }} />
             <Text style={styles.label}>Email</Text>
             <TextInput
                 style={styles.input}
@@ -21,8 +21,8 @@ export default function Login({ navigation }) {
             <Button
                 title="Login"
                 color="#000"
-                style={{ marginTop: 40, marginBottom: 40 }} 
-                onPress={() => navigation.navigate("CoachDashboard") }
+                style={{ marginTop: 40, marginBottom: 40 }}
+                onPress={() => navigation.navigate("CustomerDashboard")}
             />
             <Text style={styles.labeLink}>Forgot Password?</Text>
             <Text style={styles.labeLink}>Create Account?</Text>
@@ -45,14 +45,14 @@ const styles = StyleSheet.create({
         fontSize: 18,
         color: '#000',
         paddingTop: 10,
-        paddingBottom: 0  
+        paddingBottom: 0
     },
 
     labeLink: {
         fontSize: 14,
         textAlign: 'center',
         color: "#000",
-       padding: 10,
-       cursor: 'pointer'
+        padding: 10,
+        cursor: 'pointer'
     }
 });
