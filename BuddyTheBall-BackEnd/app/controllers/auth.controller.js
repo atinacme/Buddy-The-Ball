d8ee8a74cd469566647397acc490ad70f938b436
atinacme <atingupta@acmeintech.in>
2022-12-02 19:06:17 +0530

    mongo api 02/12/2022

--- BuddyTheBall-BackEnd/app/controllers/auth.controller.js
+++ BuddyTheBall-BackEnd/app/controllers/auth.controller.js
@@ -8,7 +8,7 @@ var bcrypt = require("bcryptjs");
 
 exports.signup = (req, res) => {
     const user = new User({
-        username: req.body.username,
+        // username: req.body.username,
         email: req.body.email,
         password: bcrypt.hashSync(req.body.password, 8)
     });
