0e7f86e8e660c7f5f70d7a28b785e46bb8f0d5bf
atinacme <atingupta@acmeintech.in>
2023-02-04 19:00:12 +0530

    changes for calendar commited 04/02/2023

--- BuddyTheBall-FrontEnd/src/coach/CoachCalendar.js
+++ BuddyTheBall-FrontEnd/src/coach/CoachCalendar.js
@@ -1,16 +1,37 @@
-import React from 'react'
+import React from 'react';
 import {
     SafeAreaView,
     TouchableOpacity,
     StyleSheet,
     Text,
+    Button,
 } from 'react-native';
-import { Agenda } from 'react-native-calendars';
+// import { Agenda } from 'react-native-calendars';
+import ApiCalendar from 'react-google-calendar-api';
+
+const config = {
+    "clientId": "102419677833205658481",
+    "apiKey": "AIzaSyD-TcDcUYtfFKABjO-VlbtI2HoOsXqFKX8",
+    "scope": "https://www.googleapis.com/auth/calendar",
+    "discoveryDocs": [
+        "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"
+    ]
+};
+
+const apiCalendar = new ApiCalendar(config);
 
 export default function CoachCalendar() {
+
+    function handleItemClick(e, name) {
+        if (name === 'sign-in') {
+            apiCalendar.handleAuthClick();
+        } else if (name === 'sign-out') {
+            apiCalendar.handleSignoutClick();
+        }
+    }
     return (
         <SafeAreaView style={styles.container}>
-            <Agenda
+            {/* <Agenda
                 selected="2022-12-01"
                 items={{
                     '2022-12-01': [{ name: 'Cycling' }, { name: 'Walking' }, { name: 'Running' }],
@@ -21,9 +42,21 @@ export default function CoachCalendar() {
                         <Text style={styles.itemText}>{item.name}</Text>
                     </TouchableOpacity>
                 )}
+            /> */}
+            <Button
+                title="Sign In"
+                color="#000"
+                style={{ marginTop: 40, marginBottom: 40 }}
+                onPress={(e) => handleItemClick(e, 'sign-in')}
+            />
+            <Button
+                title="Sign Out"
+                color="#000"
+                style={{ marginTop: 40, marginBottom: 40 }}
+                onPress={(e) => handleItemClick(e, 'sign-out')}
             />
         </SafeAreaView>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
